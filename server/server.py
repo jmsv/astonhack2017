155dcca056b11cbd6eeddb0634b81ddead6465e5
James Vickery <dev@jamesvickery.net>
2017-11-11 20:14:29 +0000

    API key environment variable

--- server/server.py
+++ server/server.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+
+
+api_key_name = "MAJESTIC_API_KEY"
+API_KEY = os.environ[api_key_name]
+print("%s: %s" % (api_key_name, API_KEY))
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
-
