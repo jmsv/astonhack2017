054d823627deb8721a0da083ca9dcfcd9493806c
Jonathan Marsh <j.r.marsh@ntlworld.com>
2017-11-11 18:04:25 +0000

    Basic Chrome Extension

---
+++ ChromeExtension/background.js
@@ -0,0 +1,4 @@
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+	var activeTab = tabs[0];
+	chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+});
---
+++ ChromeExtension/content.js
@@ -0,0 +1,16 @@
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if( request.message === "clicked_browser_action" ) {
+			var firstHref = $("a[href^='http']").eq(0).attr("href");
+			$.getJSON("http://faktnews/", {url: firstHref}, function(data) {
+				console.log(data);
+			})
+			.done(function(data, textStatus, jqXHR) { 
+				console.log( "second success" ); 
+			})
+			.fail(function(jqXHR, textStatus, errorThrown) { 
+				console.log( "error" ); 
+			});
+		}
+	}
+);
