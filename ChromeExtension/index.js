7a7aed579e6c59b21da584aefd72261f316deb93
Jonathan Marsh <j.r.marsh@ntlworld.com>
2017-11-12 02:40:56 +0000

    Pie chart working

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -4,8 +4,8 @@ function animate(){
 		animation: 1,
 		animationStep: 5,
 		multiPercentage: 0,
-		progressColor: { 0: '#FF0000', 50: '#FFA500', 90: '#00FF00'},
-		iconPosition: 'middle'
+		progressColor: { 0: '#FF0000', 50: '#FFA500', 90: '#00DD00'},
+	
 	});
 }
 var busy = false;
@@ -14,8 +14,8 @@ $(document).ready(function(){
 	$('#circles a').click(function(){
 		if(busy == false){
 			busy = true;
-			$('#circles').animate({ marginLeft: "100%", easing: "swing"} , 1000);
-			$('.page').animate({ marginLeft: "0%", easing: "swing"} , 1000, function(){busy = false;});
+			$('#circles').animate({ marginLeft: "100%", easing: "swing"} , 500);
+			$('.page').animate({ marginLeft: "0%", easing: "swing"} , 500, function(){resizeEvent(); busy = false;});
 			$(".animate").empty();
 			animate();
 		}
@@ -28,10 +28,22 @@ $(document).ready(function(){
 	$('.page a').click(function(){
 		if(busy == false){
 			busy = true;
-			$('#circles').animate({ marginLeft: "0%", easing: "swing"} , 1000);
-			$('.page').animate({ marginLeft: "-100%", easing: "swing"} , 1000,  function(){$(".page").hide();busy = false;});
+			$('#circles').animate({ marginLeft: "0%", easing: "swing"} , 500);
+			$('.page').animate({ marginLeft: "-100%", easing: "swing"} , 500,  function(){$(".page").hide();resizeEvent();busy = false;});
 			$(".animate").empty();
 			animate();
 		}
 	});
+	
+	$(".example").piechart([
+		["Word Type", "% of words"],
+		["Verb", 35],
+		["Adjective", 25],
+		["Noun", 15],
+		["Other", 25]
+	]);
+	$(".piechart-flatmin").on('mouseenter','.sector-s',hoverState);
+	$(".piechart-flatmin").on('mouseleave','.sector-s',hoverState);
+	$(".piechart-flatmin").on('click','.sector-s',clickState);
+	$(window).resize(resizeEvent);
 });
