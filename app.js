a545f35347613870bb8d0f027b206b31c08806ab
Jonathan Marsh <j.r.marsh@ntlworld.com>
2017-12-18 01:13:07 +0000

    Almost completed

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ var listenOnPort = 8082,
     app = express(),
     http = require('http').Server(app),
     request = require('request'),
-    fs = require("fs");
+    fs = require("fs"),
+    extractor = require('unfluff'),
+    emotional = require("emotional"),
+    WordPOS = require('wordpos'),
+    wordpos = new WordPOS();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -29,7 +33,69 @@ if (app.get('env') === 'development') {
     });
 }
 app.get('/stats', function (req, res) {
+    var domain = req.get("host"), result = {};
+    API_KEY = process.env.API_KEY;
+    url = `developer.majestic.com/api/json?app_api_key=${API_KEY}&cmd=GetIndexItemInfo&items=1&item0=${domain}&datasource=fresh`;
+    result.majestic = request.get(url, options, function (err, res, body) {
+        if (err) {
+            console.log("Get request failed: " + err);
+            return false;
+        }
+        if (res.statusCode !== 200)
+            try {
+                data = JSON.parse(body);
+                if (data['Code'] != 'OK') {
+                    console.log("Error getting JSON from URL");
+                    return false;
+                } else {
+                    majestic_data = data['DataTables']['Results']['Data'][0];
+                    response_data = {
+                        'CitationFlow': majestic_data['CitationFlow'],
+                        'TrustFlow': majestic_data['TrustFlow'],
+                        'Topic': majestic_data['TopicalTrustFlow_Topic_0'],
+                        'TopicValue': majestic_data['TopicalTrustFlow_Value_0']
+                    }
+                    return response_data;
+                }
+            } catch (e) {
+                console.log("Could not parse into JSON: " + data);
+                return false;
+            }
+    });
+    
+    article = extractor.lazy(req.get("host"), 'en');
+    result['Betteridge_legal'] = article.softTitle.substring(article.softTitle.length - 1) == "?";
 
+    var grammar = wordpos.getPOS(article.text);
+    result['Grammar'] = { 'Noun': grammar.noun.length, 'Verb': grammar.verb.length, 'Adjective': grammar.adjective.length, 'Adverb': grammar.adverb.length, 'Other': grammar.rest.length }
+
+    emotional.load(function () {
+        var text = emotional.get(article.text);
+        result['Polarity'] = text.polarity;
+        result['Subjectivity'] = text.subjectivity;
+        var content_veracity_coefficient = 100 - (text.subjectivity / 2) - (text.polarity / 4);
+        if (!result['Betteridge_legal'])
+            content_veracity_coefficient /= 4;
+        result['CVC'] = round(content_veracity_coefficient, 2)
+    });    
+
+    var votes = require("./votes.json");
+    result['Votes'] = votes[domain].y / (votes[domain].y + votes[domain].n);
+
+    var values = [result.majestic['CitationFlow'] || 0, result.majestic['TrustFlow'] || 0, result['CVC'] || 0, result['Votes'] || 0];
+    result.overall = 0;
+    for (var item in values) result.overall += item;
+    result.overall /= values.length;
+    switch (Math.floor(avg/15)) {
+        case (6): result.overall = 'A'; break;
+        case (5): result.overall = 'B'; break;
+        case (4): result.overall = 'C'; break;
+        case (3): result.overall = 'D'; break;
+        case (2): result.overall = 'E'; break;
+        case (1): result.overall = 'F'; break;
+        default: result.overall = 'U'; break;
+    }
+    res.json(result);
 });
 app.get('/vote', function (req, res) {
     if (req.query.trusted == "y" || req.query.trusted == "n") {
@@ -46,9 +112,11 @@ app.get('/vote', function (req, res) {
 });
 app.get('/votes', function (req, res) {
     try{
-        res.json(require("./votes.json"));
+        var votes = require("./votes.json");
+        var score = votes[req.get('host')].y / (votes[req.get('host')].y + votes[req.get('host')].n);
+        res.send(score || 0);
     } catch (e) {
-        console.log("Could not parse into JSON: " + data);
+        console.log("Could not parse file into JSON");
         res.send(false);
     }
 });
@@ -56,230 +124,3 @@ app.get('/', function (req, res) { res.sendFile(__dirname + '/index.html'); });
 app.get('*', function (req, res) { res.sendFile(__dirname + '/public/error.html'); });
 app.set('port', process.env.PORT || listenOnPort + 1);
 http.listen(app.get('port'));
-
-API_KEY = process.env.API_KEY;
-
-function get_stats(search) {
-    url = `https://developer.majestic.com/api/json?app_api_key=${API_KEY}&cmd=GetIndexItemInfo&items=1&item0=${search}&datasource=fresh`;
-    request.get(url, options, function (err, res, body) {
-        if (err) {
-            console.log("Get request failed: " + err);
-            return false;
-        }
-        if (res.statusCode !== 200) {
-            try {
-                data = JSON.parse(body);
-                if (data['Code'] != 'OK'){
-                    console.log("Error getting JSON from URL");
-                    return false;
-                }else {
-                    majestic_data = data['DataTables']['Results']['Data'][0];
-                    response_data = {
-                        'CitationFlow': majestic_data['CitationFlow'],
-                        'TrustFlow': majestic_data['TrustFlow'],
-                        'Topic': majestic_data['TopicalTrustFlow_Topic_0'],
-                        'TopicValue': majestic_data['TopicalTrustFlow_Value_0']
-                    }
-                    return response_data;
-                }
-            } catch (e) {
-                console.log("Could not parse into JSON: " + data);
-                return false;
-            }
-        }
-    });
-}
-function get_article(url) {
-    try:
-    html = urllib.request.urlopen(url).read()
-    except:
-    return ""
-    soup = BeautifulSoup(html, 'lxml')
-
-    for script in soup(["script", "style"]):
-        script.extract()
-
-    text = soup.get_text()
-    lines = (line.strip() for line in text.splitlines())
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-            text = '\n'.join(chunk for chunk in chunks if chunk)
-                cuttings = text.split('\n')
-
-    main_cuts = [cut for cut in cuttings if len(cut) > 100]
-
-    article = '\n'.join(main_cuts)
-    return article
-}
-punctuation = list('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
-punctuation.extend(['\'\'', '``'])
-
-def get_grammar_census(url):
-    article_tokens = nltk.word_tokenize(get_article(url))
-    article_tagged = nltk.pos_tag(article_tokens)
-    grammar = {}
-    for word in article_tagged:
-        if word[1] not in punctuation:
-            if word[1] not in grammar:
-                grammar[word[1]] = 1
-            else:
-                grammar[word[1]] += 1
-
-    basic_grammar = {'Noun': 0, 'Verb': 0, 'Adjective': 0, 'Adverb': 0, 'Other': 0}
-    for word_type in grammar:
-        if word_type[0] == 'N':
-            basic_grammar['Noun'] += grammar[word_type]
-        elif word_type[0] == 'V':
-            basic_grammar['Verb'] += grammar[word_type]
-        elif word_type[0] == 'J':
-            basic_grammar['Adjective'] += grammar[word_type]
-        elif word_type[0] == 'R':
-            basic_grammar['Adverb'] += grammar[word_type]
-        else:
-            basic_grammar['Other'] += grammar[word_type]
-
-    grammar_total = sum(basic_grammar.values())
-    for word_type in basic_grammar:
-        basic_grammar[word_type] *= (100 / grammar_total)
-
-    return basic_grammar
-
-
-def remove_quotes(article):
-    single_quotes = r'\[.*?\]'
-    double_quotes = r'\(.*?\)'
-    article = re.sub(single_quotes, '', article)
-    article = re.sub(double_quotes, '', article)
-    return article
-
-
-def get_polarity(article):
-    article_blob = TextBlob(article)
-    polarity = abs(article_blob.sentiment.polarity) * 100
-    return polarity
-
-
-def get_subjectivity(article):
-    article_blob = TextBlob(article)
-    subjectivity = abs(article_blob.sentiment.subjectivity) * 100
-    return subjectivity
-
-
-def betteridge_legal(url):
-    article = requests.get(url)
-    try:
-        article.raise_for_status()
-    except:
-        return False
-    soup = BeautifulSoup(article.text, 'lxml')
-    header = soup.find('h1')
-    try:
-        if header.text[-1] == '?':
-            return False
-    except:
-        return False
-    return True
-
-
-def dict_from_list(iterable):
-    result = {}
-    for value in iterable:
-        result[value[0]] = value[1]
-    return result
-
-
-def article_stats(url):
-    article_stats = {}
-    article = get_article(url)
-    gram_dict = get_grammar_census(BBC_chatbot_love)
-    gd_sorted = sorted(gram_dict.items(), key=operator.itemgetter(1), reverse=True)
-    article_stats['Grammar'] = dict_from_list(gd_sorted)
-    article_no_quotes = remove_quotes(article)
-    article_stats['Polarity'] = round(get_polarity(article_no_quotes), 2)
-    article_stats['Subjectivity'] = round(get_subjectivity(article_no_quotes), 2)
-    article_stats['Betteridge_legal'] = betteridge_legal(url)
-
-    content_veracity_coefficient = 100 - (article_stats['Subjectivity'] / 2) - (article_stats['Polarity'] / 4)
-    if not article_stats['Betteridge_legal']:
-        content_veracity_coefficient /= 4
-
-    article_stats['CVC'] = round(content_veracity_coefficient, 2)
-    return article_stats
-
-
-if __name__ == '__main__':
-    print(article_stats(str(sys.argv[1])))
-
-	
-	
-def domain_from_url(url):
-    try:
-        domain = url.split('/')[2]
-    except Exception:
-        try:
-            domain = url.split('/')[1]
-        except Exception:
-            try:
-                domain = url.split('/')[0]
-            except Exception:
-                domain = url
-    return domain
-
-
-function grade_from_values(values) {
-    avg = 0;
-    for (var item in values) avg += item;
-    avg /= values.length;
-    switch (avg) {
-        case (avg > 85): return 'A'; break;
-        case (avg > 70): return 'B'; break;
-        case (avg > 55): return 'C'; break;
-        case (avg > 40): return 'D'; break;
-        case (avg > 25): return 'E'; break;
-        case (avg > 85): return 'F'; break;
-        default: return 'U'; break;
-    }
-}
-
-@app.route("/v4")
-def get_stats_v4():
-    try:
-        search = request.args.get('search')
-        if not search:
-            return "Error", 400
-    except:
-        return "Error", 400
-
-    add_http = True
-    if 'http' in search:
-        if search.index('http') == 0:
-            add_http = False
-    if add_http:
-        search = 'http://' + search
-
-    maj_search = search[:]
-    maj_res = majestic.get_stats(API_KEY, maj_search)
-    if maj_res['TrustFlow'] <= maj_res['CitationFlow'] <= 0:
-        maj_search = domain_from_url(maj_search)
-        maj_res = majestic.get_stats(API_KEY, maj_search)
-    try:
-        aa_res = nlp.article_stats(search)
-        response = {**maj_res, **aa_res}
-    except ValueError:
-        response = maj_res
-
-    try:
-        response['VoteStat'] = vote_stats(domain_from_url(search.replace('www.', '')))
-    except:
-        response['VoteStat'] = 0
-
-    score_vals = [maj_res['TrustFlow']]
-    try:
-        score_vals.append(response['CVC'])
-    except:
-        pass
-
-    if response['VoteStat'] > 0:
-        score_vals.append(response['VoteStat'])
-    response['Grade'] = grade_from_values(score_vals)
-
-    return jsonify(response)
