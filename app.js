864d04b9787fdda05fea2c11031955d11492ab7b
Jonathan Marsh <j.r.marsh@ntlworld.com>
2017-12-21 22:29:54 +0000

    Parameter check

--- app.js
+++ app.js
@@ -41,80 +41,84 @@ var save = function () {
     });
 };
 app.get('/stats', function (req, res) {
-    var uri = url.parse(decodeURIComponent(req.query.url)),
-        result = {},
-        waiting = 4,
-        options = {
-            app_api_key: process.env.API_KEY,
-            cmd: "GetIndexItemInfo",
-            items: 1,
-            item0: uri.href,
-            datasource: "fresh"
-        };
-    request({ url: `http://developer.majestic.com/api/json`, qs: options }, function (err, res, body) {
-        if (err)
-            console.log("Get request failed: " + err);
-        if (res.statusCode === 200)
-            try {
-                data = JSON.parse(body);
-                if (data['Code'] !== 'OK')
-                    console.log("Majestic returned non-OK status");
-                else {
-                    var majestic_data = data['DataTables']['Results']['Data'][0];
-                    result['CitationFlow'] = majestic_data['CitationFlow'];
-                    result['TrustFlow'] = majestic_data['TrustFlow'];
-                    result['Topic'] = majestic_data['TopicalTrustFlow_Topic_0'];
-                    result['TopicValue'] = majestic_data['TopicalTrustFlow_Value_0'];
+    if (req.query.url) {
+        var uri = url.parse(decodeURIComponent(req.query.url)),
+            result = {},
+            waiting = 4,
+            options = {
+                app_api_key: process.env.API_KEY,
+                cmd: "GetIndexItemInfo",
+                items: 1,
+                item0: uri.href,
+                datasource: "fresh"
+            };
+        request({ url: `http://developer.majestic.com/api/json`, qs: options }, function (err, res, body) {
+            if (err)
+                console.log("Get request failed: " + err);
+            if (res.statusCode === 200)
+                try {
+                    data = JSON.parse(body);
+                    if (data['Code'] !== 'OK')
+                        console.log("Majestic returned non-OK status");
+                    else {
+                        var majestic_data = data['DataTables']['Results']['Data'][0];
+                        result['CitationFlow'] = majestic_data['CitationFlow'];
+                        result['TrustFlow'] = majestic_data['TrustFlow'];
+                        result['Topic'] = majestic_data['TopicalTrustFlow_Topic_0'];
+                        result['TopicValue'] = majestic_data['TopicalTrustFlow_Value_0'];
+                    }
+                } catch (e) {
+                    console.log("Could not parse into JSON: " + body);
                 }
-            } catch (e) {
-                console.log("Could not parse into JSON: " + body);
+            if (--waiting === 0) callback();
+        });
+
+        request({ method: 'GET', url: uri.href }, function (err, res, body) {
+            if (err)
+                console.log("Get request failed: " + err);
+            if (res.statusCode === 200) {
+                var article = unfluff(body, 'en');
+                if (article) {
+                    var title = article.softTitle;
+                    result['Betteridge'] = title.substring(title.length - 1) !== "?";
+
+                    wordpos.getPOS(article.text, function (res) {
+                        result['Grammar'] = { 'Nouns': res.nouns.length, 'Verbs': res.verbs.length, 'Adjectives': res.adjectives.length, 'Adverbs': res.adverbs.length, 'Other': res.rest.length };
+                        if (--waiting === 0) callback();
+                    });
+
+                    emotional.load(function () {
+                        var text = emotional.get(article.text);
+                        result['Polarity'] = text.polarity;
+                        result['Subjectivity'] = text.subjectivity;
+                        if (--waiting === 0) callback();
+                    });
+                }
             }
-        if (--waiting === 0) callback();
-    });
+            if (--waiting === 0) callback();
+        });
 
-    request({ method: 'GET', url: uri.href }, function (err, res, body) {
-        if (err)
-            console.log("Get request failed: " + err);
-        if (res.statusCode === 200) {
-            var article = unfluff(body, 'en');
-            if (article) {
-                var title = article.softTitle;
-                result['Betteridge'] = title.substring(title.length - 1) !== "?";
-
-                wordpos.getPOS(article.text, function (res) {
-                    result['Grammar'] = { 'Nouns': res.nouns.length, 'Verbs': res.verbs.length, 'Adjectives': res.adjectives.length, 'Adverbs': res.adverbs.length, 'Other': res.rest.length };
-                    if (--waiting === 0) callback();
-                });
-
-                emotional.load(function () {
-                    var text = emotional.get(article.text);
-                    result['Polarity'] = text.polarity;
-                    result['Subjectivity'] = text.subjectivity;
-                    if (--waiting === 0) callback();
-                });
-            }
+        var domain = uri.hostname.replace(/^www\./, '');
+        if (votes[domain]) {
+            result['VotesFor'] = votes[domain].y;
+            result['VotesAgainst'] = votes[domain].n;
         }
-        if (--waiting === 0) callback();
-    });
 
-    var domain = uri.hostname.replace(/^www\./, '');
-    if (votes[domain]) {
-        result['VotesFor'] = votes[domain].y;
-        result['VotesAgainst'] = votes[domain].n;
-    }
-
-    function callback() {
-        res.json(result);
-    }
+        function callback() {
+            res.json(result);
+        }
+    } else res.json({ Error:"URI Parameter missing"});
 });
 app.get('/vote', function (req, res) { 
-    if (req.query.trusted === "y" || req.query.trusted === "n") {
-        var uri = url.parse(decodeURIComponent(req.query.url)).hostname.replace(/^www\./, '');
-        if (!votes[uri]) votes[uri] = { "y": 0, "n": 0 };
-        votes[uri][req.query.trusted]++;
-        res.json(votes[uri]);
-        debounce(save, 1000);
-    } 
+    if (req.query.trusted && req.query.url) {
+        if (req.query.trusted === "y" || req.query.trusted === "n") {
+            var uri = url.parse(decodeURIComponent(req.query.url)).hostname.replace(/^www\./, '');
+            if (!votes[uri]) votes[uri] = { "y": 0, "n": 0 };
+            votes[uri][req.query.trusted]++;
+            res.json(votes[uri]);
+            debounce(save, 1000);
+        }
+    } else res.json({ Error: "Parameter missing" });
 });
 app.get('/', function (req, res) { res.sendFile(__dirname + '/public/index.html'); });
 app.get('*', function (req, res) { res.sendFile(__dirname + '/public/error.html'); });
